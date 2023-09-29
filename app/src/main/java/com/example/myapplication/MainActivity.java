5b963ddd2a49659a86cf96b38ff74ae8d5390f29
gurxt <jung_jungian@outlook.com>
2023-09-29 11:24:09 -0400

    step 3 (relative layout)

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -18,8 +18,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 //        setContentView(R.layout.activity_main);
-//        setContentView(R.layout.activity_main_relative);
-        setContentView(R.layout.activity_main_grid);
+        setContentView(R.layout.activity_main_relative);
+//        setContentView(R.layout.activity_main_grid);
 //        setContentView(R.layout.activity_main_linear);
 
         Button btn = findViewById(R.id.button);
